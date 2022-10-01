dfee2416e0b7b7b5048a55bc42d41a843752f46f
Remi Pince <remi.pince@outlook.com>
2022-10-01 13:16:27 +0200

    ajout lien vers repo cv

--- src/components/AboutView.vue
+++ src/components/AboutView.vue
@@ -1,9 +1,9 @@
 <template>
   <section
     id="about"
-    class="flex flex-col xl:flex-row justify-center items-center w-full h-128 relative z-10 min-h-screen bg-white"
+    class="flex flex-col xl:flex-row justify-center items-center w-full relative z-10 min-h-screen bg-white"
   >
-    <div class="block md:w-3/5 w-full mt-16">
+    <div class="block md:justify-center md:w-3/5 w-full mt-16">
       <h2
         class="m-7 text-4xl md:text-5xl border-b-4 md:border-b-0 p-1 font-bold"
       >
@@ -12,31 +12,29 @@
       <h3 class="m-7 text-2xl md:text-4xl italic">
         Un étudiant passionné de développement
       </h3>
-      <p class="m-7 text-base italic">
+      <p class="m-7 md:m-auto text-base italic md:w-1/2">
         Je m'appelle Rémi, j'ai 25 ans, titulaire d'un bachelor d'école de
         commerce, je suis actuellement en formation de développeur web che
         OpenClassrooms. Je recherche une alternance pour commencer ma prochaine
         formation : Developpeur frontend JavaScript et React.
       </p>
-      <p class="m-7 text-base italic">
+      <p class="m-7 md:m-auto text-base italic md:w-1/2">
         Ayant toujours été passionné par l'informatique, j'ai décidé de me
         lancer dans cette formation afin de me spécialiser dans le développement
         web, et de pouvoir exercer un métier qui me passionne.
       </p>
       <article class="justify-center wrap">
         <h3 class="m-7 text-2xl md:text-3xl font-bold italic">Mon CV</h3>
-        <div
-          v-for="(filesrc, index) in filesrcs"
-          :key="index"
-          class="cursor-pointer"
+        <a
+          href="https://github.com/Osiri70cl/RemiP/blob/main/CV-R%C3%A9miP.pdf"
+          rel="noopener noreferrer"
+          target="_blank"
         >
-          <div @click="downloadWithAxios(filesrc.src, filesrc.title)">
-            <p>{{ filesrc.title }}</p>
-          </div>
-        </div>
+          CV-Rémi.P.pdf
+        </a>
       </article>
     </div>
-    <div class="sm:w-2/5 sm:visible invisible">
+    <div class="sm:w-2/5 sm:visible invisible sm-2 p-4">
       <img
         src="../images/paupy.jpg"
         alt="photo des pyrénées"
@@ -46,41 +44,4 @@
   </section>
 </template>
 
-<script>
-import axios from "axios";
-export default {
-  name: "AboutView",
-  data() {
-    return {
-      filesrcs: [
-        {
-          title: "cv rémi.P",
-          src: require("../images/cvplaceholder.png"),
-        },
-      ],
-    };
-  },
-  methods: {
-    forceFileDownload(response, title) {
-      console.log(title);
-      const url = window.URL.createObjectURL(new Blob([response.data]));
-      const link = document.createElement("a");
-      link.href = url;
-      link.setAttribute("download", title);
-      document.body.appendChild(link);
-      link.click();
-    },
-    downloadWithAxios(url, title) {
-      axios({
-        method: "get",
-        url,
-        responseType: "arraybuffer",
-      })
-        .then((response) => {
-          this.forceFileDownload(response, title);
-        })
-        .catch(() => console.log("error occured"));
-    },
-  },
-};
-</script>
+<script></script>
